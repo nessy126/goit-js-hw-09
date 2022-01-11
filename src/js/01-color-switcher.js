8ac0a4f341ab049c81ca2d7510210f95c9c8c48a
nessy126 <nessy126@gmail.com>
2022-01-11 18:49:37 +0300

    01 task completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const btnStartEl = document.querySelector('[data-start]');
+const btnStopEl = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+btnStartEl.addEventListener('click', startSwitch);
+btnStopEl.addEventListener('click', stopSwitch);
+let changeColor = null
+
+function startSwitch() {
+  changeColor = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    btnStartEl.disabled = true;
+  }, 1000);
+}
+
+function stopSwitch() {
+  btnStartEl.disabled = false;
+  clearInterval(changeColor)
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
